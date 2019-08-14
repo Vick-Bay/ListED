3574921622890c09ba6ff07b2642fa3cbc7c701c
Vick-Bay <50940714+Vick-Bay@users.noreply.github.com>
2019-08-14 19:07:19 +0400

    latest version

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
-var input = document.getElementById("userinput");
+var input = document.getElementById("userInput");
+var button = document.getElementById("enterButton");
+var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li");
+
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    li.classList.add("blue");
+    ul.appendChild(li);
+    input.value = "";
+
+    // Crossed out when clicked
+
+    li.addEventListener("click", function () {
+        li.classList.toggle("done");
+    });
+
+
+    // Create a new delete button to remove item from the list
+    var button = document.createElement("button");
+    button.appendChild(document.createTextNode("Delete!"));
+    li.appendChild(button);
+    button.onclick = removeParent;
+}
+
+function removeParent(event) {
+    event.target.parentNode.remove();
+}
+
+function addListAfterClick() {
+    if (input.value.length > 0) {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if (input.value.length > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
 
